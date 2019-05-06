76ca18d3add69e5196ccf8b93c05f2023ef2fb22
cestbon258 <qijiang7-c@my.cityu.edu.hk>
2019-05-06 17:19:39 +0800

    water switch

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = express()
 const port = 3000 // current port
 const path = require('path');
 let ejs = require('ejs')
- 
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());
+
+// let public directory accessable 
 app.use(express.static(__dirname + '/public'));
 
 // set the view engine to ejs
@@ -26,11 +31,12 @@ app.get('/', function (req, res) {
 
 // monitoring center
 app.get('/user-center', function (req, res, next) {
-	connection.query('SELECT * FROM data;', function (error, results, fields) {
+	// connection.query('SELECT * FROM data;', function (error, results, fields) {
 		// if (error) throw error;
 		// console.log('The solution is: ', results);
-		res.render('pages/index', {results: results});
-	});
+		// res.render('pages/index', {results: results});
+		res.render('pages/index');
+	// });
 })
 
 app.get('/poll-moisture-data', function (req, res, next) {
@@ -80,5 +86,35 @@ app.get('/product', function (req, res, next){
 app.get('/help', function (req, res, next) {
 	res.render('pages/help');
 })
+
+app.get('/settings', function (req, res, next) {
+	// var tb_email=req.query;
+	// console.log(req.query.test);
+	try {
+		connection.query('SELECT status FROM switch LIMIT 1 ', function (error, results, fields) {
+			// if (error) throw error;
+			console.log('The solution is: ', results);
+			res.render('pages/settings', {results: results});
+			
+		});
+	} catch(err) {
+		console.log(err);
+	}
+})
+
+app.post('/control', function (req, res, next) {
+	var test = req.body.water == "on" ? 1 : 0;
+	try {
+		connection.query('UPDATE switch SET status = ? WHERE id=1', [test], function (error, results, fields) {
+			// if (error) throw error;
+			console.log('The solution is: ', results);
+			res.redirect('settings');
+			
+		});
+	} catch(err) {
+		console.log(err);
+	}
+})
+
 // connection.end();
 app.listen(port, () => console.log(`App listening on port: ${port}!`));
