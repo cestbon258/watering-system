63e94d72f5fdcb81d84117c2694523693a84676f
cestbon258 <qijiang7-c@my.cityu.edu.hk>
2019-05-12 17:24:02 +0800

    create user login ang register

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ const app = express()
 const port = 3000 // current port
 const path = require('path');
 let ejs = require('ejs')
-
 const bodyParser = require('body-parser');
+
+const session = require('express-session')
+
+
+app.use(session({
+	secret: 'a8ab97d5-bc71-4102-b33a-3e84c86e1502',
+	resave: true,
+	saveUninitialized: true,
+	cookie: { maxAge: 60000 }
+}))
 app.use(bodyParser.urlencoded({ extended: false }));  
 app.use(bodyParser.json());
 
@@ -26,17 +35,21 @@ try {
 }
 // index
 app.get('/', function (req, res) {
-  	res.render('pages/indexx');
+  	res.render('pages/indexx', {is_login: req.session.email});
 });
 
 // monitoring center
 app.get('/user-center', function (req, res, next) {
+	if (req.session.email) {
 	// connection.query('SELECT * FROM data;', function (error, results, fields) {
 		// if (error) throw error;
 		// console.log('The solution is: ', results);
 		// res.render('pages/index', {results: results});
-		res.render('pages/index');
+		res.render('pages/index', {is_login: req.session.email});
 	// });
+	} else {
+		res.redirect('login');
+	}
 })
 
 app.get('/poll-moisture-data', function (req, res, next) {
@@ -80,25 +93,129 @@ app.get('/poll-temperature-data', function (req, res, next) {
 
 // introduction to the system
 app.get('/product', function (req, res, next){
-	res.render('pages/product');
+	res.render('pages/product', {is_login: req.session.email});
 })
 
 app.get('/help', function (req, res, next) {
-	res.render('pages/help');
+	res.render('pages/help', {is_login: req.session.email});
+})
+
+app.get('/store', function (req, res, next) {
+	res.render('pages/shop', {is_login: req.session.email});
+})
+
+app.get('/login', function (req, res, next) {
+	res.render('pages/login', {is_login: req.session.email});
+})
+
+app.post('/user-login', function (req, res, next) {
+	if (req.body.email == ""){
+		res.render('pages/login');
+	}
+	if (req.body.password == ""){
+		res.render('pages/login');
+	} 
+
+	try {
+		connection.query('SELECT * FROM users WHERE email = ? LIMIT 1', [req.body.email], function (error, results, fields) {
+			if (results[0]){
+				connection.query('SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1', [req.body.email, req.body.password], function (error, results, fields) {
+					if (results[0]){ 
+						req.session.email = results[0]["email"];
+						console.log('The solution is: ', results);
+						res.redirect('/');
+					} else {
+						res.render('pages/login');
+					}
+				})
+			} else {
+				res.render('pages/login');
+			}
+		})
+	} catch(err) {
+		console.log(err);
+	}
+
+	// try {
+	// 	connection.query('SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1', [req.body.email, req.body.password], function (error, results, fields) {
+	// 		req.session.email = results[0]["email"];
+	// 		// console.log(req.session.email);
+	// 		// console.log(results[0]["email"]);
+	// 		console.log('The solution is: ', results);
+	// 		res.redirect('/');
+	// 	});
+	// } catch(err) {
+	// 	res.redirect('login');
+	// }
+
+})
+
+app.get('/signup', function (req, res, next) {
+	res.render('pages/register', {is_login: req.session.email});
+})
+
+app.post('/user-register', function (req, res, next) {
+	if (req.body.email == ""){
+		res.render('pages/register');
+	}
+
+	try{
+		connection.query('SELECT * FROM users WHERE email = ? LIMIT 1', [req.body.email], function (error, results, fields) {
+			if (results[0]) {
+				console.log("email has been taken");
+				res.render('pages/register');
+			}
+		});
+	} catch (err) {
+		console.log(err);
+	}
+
+	if (req.body.password == ""){
+		res.render('pages/register');
+	} 
+	if (req.body.password2 == ""){
+		res.render('pages/register');
+	} 
+	if (req.body.password != req.body.password2){
+		console.log("passwords do not match");
+		res.render('pages/register');
+	} 
+
+	try {
+		connection.query('INSERT INTO users (email, password) VALUES (?, ?)', [req.body.email, req.body.password], function (error, results, fields) {
+			if (error) throw error;
+			res.redirect('login');
+		});
+	} catch(err) {
+		console.log(err);
+	}
+
+	// res.render('pages/register', {is_login: req.session.email});
+})
+
+app.get('/signout', function (req, res, next) {
+	req.session.destroy(function(err) {
+		// cannot access session here
+	})
+	res.redirect('/');
 })
 
 app.get('/settings', function (req, res, next) {
 	// var tb_email=req.query;
 	// console.log(req.query.test);
-	try {
-		connection.query('SELECT status FROM switch LIMIT 1 ', function (error, results, fields) {
-			// if (error) throw error;
-			console.log('The solution is: ', results);
-			res.render('pages/settings', {results: results});
-			
-		});
-	} catch(err) {
-		console.log(err);
+	if (req.session.email) {
+		try {
+			connection.query('SELECT status FROM switch LIMIT 1 ', function (error, results, fields) {
+				// if (error) throw error;
+				console.log('The solution is: ', results);
+				res.render('pages/settings', {is_login: req.session.email,results: results});
+				
+			});
+		} catch(err) {
+			console.log(err);
+		}
+	} else {
+		res.redirect('login');
 	}
 })
 
